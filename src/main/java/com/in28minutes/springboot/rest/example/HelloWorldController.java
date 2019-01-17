b49572a571a911d4fd2f9026de779a43ec12b809
khansuhl <khansuhl@gmail.com>
2019-01-17 16:45:04 +0530

    first commit

---
+++ src/main/java/com/in28minutes/springboot/rest/example/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.in28minutes.springboot.rest.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping("/")
+	public  String printInBrowser() {
+		return "Hello World from Bridgelabz";
+	}
+	
+}
---
+++ src/main/java/com/in28minutes/springboot/rest/example/SpringBoot2HelloWorldApplication.java
@@ -0,0 +1,12 @@
+package com.in28minutes.springboot.rest.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2HelloWorldApplication.class, args);
+	}
+}
