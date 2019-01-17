ab0ee89bf857a8ee80320f40c9b900714cd74774
khansuhl <khansuhl@gmail.com>
2019-01-17 18:08:03 +0530

    third commit

--- src/main/java/com/in28minutes/springboot/rest/example/HelloWorldController.java
+++ src/main/java/com/in28minutes/springboot/rest/example/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
 	@GetMapping("/")
 	public  String printInBrowser() {
-		return "Hello World from Bridgelabz and Suhail";
+		return "Hello World from Bridgelabz";
 	}
 	
 }
