461fb6f8a81f261ae7ccf3828c76e1fed6a9c40e
Suhail Ahmad Khan <khansuhl@gmail.com>
2019-03-19 17:27:19 +0530

    demo trial

--- src/main/java/com/in28minutes/springboot/rest/example/HelloWorldController.java
+++ src/main/java/com/in28minutes/springboot/rest/example/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
 	@GetMapping("/")
 	public  String printInBrowser() {
-		return "Hello World from Bridgelabz from the DEVOPS team";
+		return "Hello World from Bridgelabz";
 	}
 	
 }
