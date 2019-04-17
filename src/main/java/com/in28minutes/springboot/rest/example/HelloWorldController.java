27612d9f9be8ec0cdcb0aab6fa178c917ae700c3
bridgeit <bridgeit@localhost.localdomain>
2019-04-17 16:06:37 +0530

    vaibhav new commit

--- src/main/java/com/in28minutes/springboot/rest/example/HelloWorldController.java
+++ src/main/java/com/in28minutes/springboot/rest/example/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
 	@GetMapping("/")
 	public  String printInBrowser() {
-		return "Hello World from Bridgelabz and Yuvraj";
+		return "Hello World from Bridgelabz and Vaibhav";
 	}
 	
 }
