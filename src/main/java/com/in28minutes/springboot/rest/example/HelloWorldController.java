b8a90bcfaaf2f4a2cc87be0189854171c0da2ca7
suhail khan <khansuhl@gmail.com>
2019-05-14 16:06:58 +0530

    Update HelloWorldController.java

--- src/main/java/com/in28minutes/springboot/rest/example/HelloWorldController.java
+++ src/main/java/com/in28minutes/springboot/rest/example/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
 	@GetMapping("/")
 	public  String printInBrowser() {
-		return "Hello World from Bridgelabz from Bhupendra and Akshay";
+		return "Hello World from Bridgelabz";
 	}
 	
 }
