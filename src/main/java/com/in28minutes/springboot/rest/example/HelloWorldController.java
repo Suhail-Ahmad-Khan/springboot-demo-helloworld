c36d3e3a1fd39fb5f1d040ea5b81d7a04295ee2f
suhail khan <khansuhl@gmail.com>
2020-07-15 12:38:05 +0530

    Hello World from Bridgelabz
    Hello World from Bridgelabz

--- src/main/java/com/in28minutes/springboot/rest/example/HelloWorldController.java
+++ src/main/java/com/in28minutes/springboot/rest/example/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
 	@GetMapping("/")
 	public  String printInBrowser() {
-		return "Hello World from Bridgelabz on 15th of July";
+		return "Hello World from Bridgelabz to the engg team on 15th of July";
 	}
 	
 }
