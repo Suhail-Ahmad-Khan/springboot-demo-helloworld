f9c0990606b5066456697302573e6551195dbedf
suhail khan <khansuhl@gmail.com>
2020-07-15 12:35:39 +0530

    code updated on 15th of July
    code updated on 15th of July

--- src/main/java/com/in28minutes/springboot/rest/example/HelloWorldController.java
+++ src/main/java/com/in28minutes/springboot/rest/example/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
 	@GetMapping("/")
 	public  String printInBrowser() {
-		return "Hello World on 13th July 2020";
+		return "Hello World from Bridgelabz on 15th of July";
 	}
 	
 }
